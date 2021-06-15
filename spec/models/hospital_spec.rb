baf1bc90d296caff1db5dcaf557f44487fb36017
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-06-15 18:25:12 +0200

    Creates model test for hospital

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Hospital do
     it { should have_many(:doctors) }
     it {should have_many(:patients).through(:doctors)}
   end
+
+  before(:each) do
+    @hospital = Hospital.create!(name: 'Saint Elsewhere')
+
+    @doc_1 = @hospital.doctors.create!(name: 'Dr. Jay', specialty: 'Surgeon', university:'Harvard')
+    @doc_2 = @hospital.doctors.create!(name: 'Dr. Kay', specialty: 'Pediatrician', university:'Yale')
+    @doc_3 = @hospital.doctors.create!(name: 'Dr. Yao', specialty: 'Eye', university:'Stanford')
+    @doc_4 = @hospital.doctors.create!(name: 'Dr. Ray', specialty: 'Ear', university:'Stanford')
+  end
+
+  describe 'instance methods' do
+    it '#university' do
+
+      expect(@hospital.university.count).to be(3)
+    end
+  end
 end
