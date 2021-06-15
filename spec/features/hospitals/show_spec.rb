a33aa59479265a421a6b7eb236a1560cdb5d5dc3
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-06-15 17:50:38 +0200

    Creates hospital feature test, adds controller

---
+++ spec/features/hospitals/show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'hospital show page' do
+  before(:each) do
+    @hospital = Hospital.create!(name: 'Saint Elsewhere')
+
+    @doc_1 = @hospital.doctors.create!(name: 'Dr. Jay', specialty: 'Surgeon', university:'Harvard')
+    @doc_2 = @hospital.doctors.create!(name: 'Dr. Kay', specialty: 'Pediatrician', university:'Yale')
+
+    @pat_1 = Patient.create!(name: 'Jeff', age: 31)
+    @pat_2 = Patient.create!(name: 'Ana', age: 27)
+    @pat_3 = Patient.create!(name: 'Gary', age: 18)
+
+    @pat_4 = Patient.create!(name: 'Jess', age: 52)
+
+    Appointment.create!(doctor:@doc_1, patient: @pat_1)
+    Appointment.create!(doctor:@doc_1, patient: @pat_2)
+    Appointment.create!(doctor:@doc_1, patient: @pat_3)
+
+    Appointment.create!(doctor:@doc_2, patient: @pat_4)
+  end
+
+  it 'displays the hospitals name' do
+    visit "/hospitals/#{@hospital.id}"
+
+    expect(page).to have_content(@hospital.name)
+  end
+end
