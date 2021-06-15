8cd2a64e0ce51352b73aa706f8dda755f52e9729
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-06-15 19:34:01 +0200

    Adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Patient.destroy_all
+Doctor.destroy_all
+Hospital.destroy_all
+
+@hospital = Hospital.create!(name: 'Saint Elsewhere')
+
+@doc_1 = @hospital.doctors.create!(name: 'Dr. Jay', specialty: 'Surgeon', university:'Harvard')
+@doc_2 = @hospital.doctors.create!(name: 'Dr. Kay', specialty: 'Pediatrician', university:'Yale')
+
+@pat_1 = Patient.create!(name: 'Jeff', age: 31)
+@pat_2 = Patient.create!(name: 'Ana', age: 27)
+@pat_3 = Patient.create!(name: 'Gary', age: 18)
+@pat_4 = Patient.create!(name: 'Jess', age: 52)
+
+Appointment.create!(doctor:@doc_1, patient: @pat_1)
+Appointment.create!(doctor:@doc_1, patient: @pat_2)
+Appointment.create!(doctor:@doc_1, patient: @pat_3)
+Appointment.create!(doctor:@doc_2, patient: @pat_4)
